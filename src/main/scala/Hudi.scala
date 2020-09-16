9ee3da76769a1c39101e6cebbd9b30fbdb344f63
ADASTRACORPNET\Jihen.Bouguerra <jihen.bouguerra@adastragrp.com>
2020-09-16 14:25:03 +0200

    write a hudi table

--- src/main/scala/Hudi.scala
+++ src/main/scala/Hudi.scala
@@ -4,29 +4,56 @@ import org.apache.hudi.config.HoodieWriteConfig.TABLE_NAME
 import org.apache.spark.sql.SaveMode.Overwrite
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.functions._
+import org.apache.hudi.DataSourceWriteOptions
+import org.apache.hudi.config.HoodieWriteConfig
+import org.apache.hudi.hive.MultiPartKeysValueExtractor
 
 object Hudi {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().
       setMaster("local").
       setAppName("Hello_world")
+    var path_data = "C:\\Users\\Jihen.Bouguerra" +
+      "\\Documents\\GitHub\\spark_hudi\\src\\data\\brut_data\\data.parquet"
+    var path_hudi_table = "C:\\Users\\Jihen.Bouguerra\\" +
+      "Documents\\GitHub\\spark_hudi\\src\\data\\hudi"
+
+
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
-    val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
+    val spark = SparkSession.builder
+      .config(sc.getConf)
+      .config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse")
+      .getOrCreate()
     import spark.implicits._
     val data = Seq(1, 2, 3, 3, 4, 5, 6)
-    val rdd = spark.sparkContext.parallelize(data)
-    val df = rdd.toDF("id")
-    df.show()
-    val tableName = "id"
-    val basePath = "\\hadoop\\hudi-table\\data.parquet"
+    val id_df = spark.sparkContext.parallelize(data).toDF()
+    id_df.show()
+
+    var people_df = spark.read.parquet(path_data)
+    people_df.show()
+    val hudiOptions = Map[String, String](
+      HoodieWriteConfig.TABLE_NAME -> "people_table",
+      DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY -> "id",
+      DataSourceWriteOptions.PRECOMBINE_FIELD_OPT_KEY -> "id"
+    )
+
+    people_df.write.
+      options(getQuickstartWriteConfigs).
+      options(hudiOptions)
+      .mode(SaveMode.Overwrite).
+      save(path_hudi_table)
 
 
+    val hudi_table = spark
+      .read
+      .format("org.apache.hudi")
+      .load(s"$path_hudi_table").show(truncate = false)
 
 
-    val addressDf = spark.read.format("hudi")
-      .text("src\\data\\addresses.csv")
-    addressDf.show()
   }
 }
 
