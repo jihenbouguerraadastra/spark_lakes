fa4e6beecbe954e488847ac88d6383e83a6e7f18
ADASTRACORPNET\Jihen.Bouguerra <jihen.bouguerra@adastragrp.com>
2020-09-16 16:43:27 +0200

    hudi delete operation

--- src/main/scala/Hudi.scala
+++ src/main/scala/Hudi.scala
@@ -1,23 +1,20 @@
+import org.apache.hudi.DataSourceWriteOptions
 import org.apache.hudi.DataSourceWriteOptions._
 import org.apache.hudi.QuickstartUtils._
-import org.apache.hudi.config.HoodieWriteConfig.TABLE_NAME
-import org.apache.spark.sql.SaveMode.Overwrite
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.functions._
-import org.apache.hudi.DataSourceWriteOptions
 import org.apache.hudi.config.HoodieWriteConfig
-import org.apache.hudi.hive.MultiPartKeysValueExtractor
+import org.apache.hudi.config.HoodieWriteConfig.TABLE_NAME
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Hudi {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().
       setMaster("local").
       setAppName("Hello_world")
-    var path_data = "\\Users\\Jihen.Bouguerra" +
+    val path_data = "\\Users\\Jihen.Bouguerra" +
       "\\Documents\\GitHub\\spark_hudi\\src\\data\\brut_data\\data.parquet"
-    var path_hudi_table = "\\Users\\Jihen.Bouguerra\\" +
+    val path_hudi_table = "\\Users\\Jihen.Bouguerra\\" +
       "Documents\\GitHub\\spark_hudi\\src\\data\\hudi"
 
 
@@ -27,45 +24,49 @@ object Hudi {
     val spark = SparkSession.builder
       .config(sc.getConf)
       .getOrCreate()
-    import spark.implicits._
-    val data = Seq(1, 2, 3, 3, 4, 5, 6)
-    val id_df = spark.sparkContext.parallelize(data).toDF()
-    id_df.show()
 
-    var people_df = spark.read.parquet(path_data)
+    /* Initialize hudi table from parquet file*/
+    val people_df = spark.read.parquet(path_data)
     people_df.show()
-    val hudiOptions = Map[String, String](
+
+
+    val hudi_options = Map[String, String](
       HoodieWriteConfig.TABLE_NAME -> "people_table",
       DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY -> "id",
       DataSourceWriteOptions.PRECOMBINE_FIELD_OPT_KEY -> "id"
     )
-
     people_df.write.
       options(getQuickstartWriteConfigs).
-      options(hudiOptions)
+      options(hudi_options)
       .mode(SaveMode.Overwrite).
       save(path_hudi_table)
 
-    var rest = people_df.filter(col("first_name") === "Amanda").
+
+    /* Apply filter on Hudi table and save changes*/
+    people_df.filter(col("first_name") === "Amanda").
       write.options(getQuickstartWriteConfigs).
       option(OPERATION_OPT_KEY, "upsert").
       option(PRECOMBINE_FIELD_OPT_KEY, "id").
       option(RECORDKEY_FIELD_OPT_KEY, "id").
-      option(TABLE_NAME, "people_table_filtred").
+      option(TABLE_NAME, "people_table_filtered").
       mode(SaveMode.Append).
       save(path_hudi_table)
 
-        val hudi_table = spark
-          .read
-          .format("org.apache.hudi")
-          .load(s"$path_hudi_table/*")
+    /* Apply delete + condition on Hudi table and save changes*/
+    people_df.filter(col("first_name") === "Amanda").
+      write.options(getQuickstartWriteConfigs).
+      option(OPERATION_OPT_KEY, "delete").
+      option(PRECOMBINE_FIELD_OPT_KEY, "id").
+      option(RECORDKEY_FIELD_OPT_KEY, "id").
+      option(TABLE_NAME, "people_table_after_delete").
+      mode(SaveMode.Append).
+      save(path_hudi_table)
 
-        hudi_table.show(truncate = false)
 
-//    var filter_df = spark.read.
-//      parquet(path_hudi_table + "\\part-00000-4f4ea92c-a928-4fa7-8dfa-96c9c06a5360-c000.snappy.parquet")
-//
-//  filter_df.show()
+    //    var filter_df = spark.read.
+    //      parquet(path_hudi_table + "\\part-00000-4f4ea92c-a928-4fa7-8dfa-96c9c06a5360-c000.snappy.parquet")
+    //
+    //  filter_df.show()
   }
 }
 
