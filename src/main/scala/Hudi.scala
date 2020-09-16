6fe0a18c2f5ea33db67ba0f0a88c60210a13083b
ADASTRACORPNET\Jihen.Bouguerra <jihen.bouguerra@adastragrp.com>
2020-09-16 12:41:08 +0200

    still trying with hudi

--- src/main/scala/Hudi.scala
+++ src/main/scala/Hudi.scala
@@ -1,22 +1,31 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.hudi.DataSourceWriteOptions._
+import org.apache.hudi.QuickstartUtils._
+import org.apache.hudi.config.HoodieWriteConfig.TABLE_NAME
+import org.apache.spark.sql.SaveMode.Overwrite
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Hudi {
   def main(args: Array[String]): Unit = {
-
     val conf = new SparkConf().
       setMaster("local").
       setAppName("Hello_world")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
     val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
-
     import spark.implicits._
-    val columns = Seq("id")
     val data = Seq(1, 2, 3, 3, 4, 5, 6)
     val rdd = spark.sparkContext.parallelize(data)
-    val dfFromRDD1 = rdd.toDF("id")
-    dfFromRDD1.show()
+    val df = rdd.toDF("id")
+    df.show()
+    val tableName = "id"
+    val basePath = "\\hadoop\\hudi-table\\data.parquet"
+
+    var df2 = spark.read.parquet(basePath)
+    df2.show()
+
+    val hoodieIncViewDF = spark.read.format("org.apache.hudi")
+      .load(basePath);
 
   }
 }
