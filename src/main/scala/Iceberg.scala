9afb9678565c3553cdcbd814747062546becb108
Mustafa <Anife.Mustafa@adastragrp.com>
2020-09-17 13:10:11 +0300

    Iceberg functions

--- src/main/scala/Iceberg.scala
+++ src/main/scala/Iceberg.scala
@@ -1,23 +1,52 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
 object Iceberg {
-    import org.apache.spark.SparkContext
-    import org.apache.spark.sql.SparkSession
+  import org.apache.spark.SparkConf
+  val conf = new SparkConf().setAppName("ICEBERG").setMaster("local[2]").set("spark.executor.memory", "1g")
+  val sc = new SparkContext(conf)
+  val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+  val spark = SparkSession
+    .builder()
+    .getOrCreate();
 
-    def main(args: Array[String]): Unit = {
-      import org.apache.spark.SparkConf
-      val conf = new SparkConf().setAppName("ICEBERG").setMaster("local[2]").set("spark.executor.memory", "1g")
-      //val conf = new SparkConf().setAppName("Simple Application")
-      val sc = new SparkContext(conf)
-      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-      val spark = SparkSession
-        .builder()
-        .getOrCreate();
+  val data_path = "C:\\AGE-Lab\\Problem-Statement-1\\iceberg-table\\addresses.csv"
+  val addressDf = spark.read.option("inferSchema","true").format("iceberg").text(data_path)
+  addressDf.createOrReplaceTempView("address")
+  //val results = sqlContext.sql("SELECT * FROM address")
+  //results.show()
 
-      val addressDf = spark.read.format("iceberg")
-        .text("src\\data\\addresses.csv")
-      addressDf.createOrReplaceTempView("address")
-      addressDf.show()
-      val results = sqlContext.sql("SELECT * FROM address")
-      //results.collect.foreach(println)
-      results.show()
-    }
+  def read_df(): Unit = {
+    val addressDf = spark.read
+      .option("inferSchema","true")
+      .format("iceberg")
+      .text(data_path)
+    addressDf.show()
   }
+
+  def write_df(): Unit = {
+    addressDf.write
+      .mode("append")
+      .save("C:\\AGE-Lab\\Problem-Statement-1\\spark_lakes\\iceberg-tables")
+  }
+
+  def filter_df(): Unit = {
+    spark
+      .sql("select * from address where value LIKE 'John%'")
+      .show()
+  }
+
+  //Row-level deletes are not supported in the current format version
+  def delete_df(): Unit = {
+    val add = spark
+      .sql("select * from address where value LIKE 'John%'")
+      .drop("value")
+  }
+
+  def main(args: Array[String]): Unit = {
+    read_df()
+    write_df()
+    filter_df()
+    delete_df()
+  }
+}
